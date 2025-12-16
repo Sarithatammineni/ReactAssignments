49ccde252593329fe48a4abc312f704c99ffaab1
Sarithatammineni <sarithatammineni18@gmail.com>
2025-12-17 00:17:12 +0530

    React component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import MessageCard from "./MessageCard";
+import UserProfile from "./UserProfile";
 
 function App() {
   return (
     <>
-      <MessageCard
-        title="Welcome"
-        message="This is the first message card."
-      />
-
-      <MessageCard
-        title="React Props"
-        message="Props allow data to be passed to components."
-      />
-
-      <MessageCard
-        title="Reusable Component"
-        message="This card is reused with different data."
-      />
-
-      <MessageCard
-        title="Success"
-        message="You have rendered multiple components!"
-      />
+      <UserProfile />
     </>
   );
 }
