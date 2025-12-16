fe686cc92b29a8fc44b180fe1d4e2bd83061d417
Sarithatammineni <sarithatammineni18@gmail.com>
2025-12-17 00:10:24 +0530

    Fourth react Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import Calculator from "./Calculator.jsx";
+import MessageCard from "./MessageCard";
+
 function App() {
   return (
-    <div>
-      <Calculator />
-    </div>
+    <>
+      <MessageCard
+        title="Welcome"
+        message="This is the first message card."
+      />
+
+      <MessageCard
+        title="React Props"
+        message="Props allow data to be passed to components."
+      />
+
+      <MessageCard
+        title="Reusable Component"
+        message="This card is reused with different data."
+      />
+
+      <MessageCard
+        title="Success"
+        message="You have rendered multiple components!"
+      />
+    </>
   );
 }
+
 export default App;
