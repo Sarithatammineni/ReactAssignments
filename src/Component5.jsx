40ccc8e30dde8e3dd06852efacd6d9a05554ea41
Sarithatammineni <sarithatammineni18@gmail.com>
2025-12-20 18:31:42 +0530

    Props Drilling

---
+++ src/Component5.jsx
@@ -0,0 +1,12 @@
+function Component5({ e, f }) {
+  return (
+    <div>
+      <h3>Component 5</h3>
+      <h4>This is prop f: {f}</h4>
+
+      <Component6 e={e} />
+    </div>
+  );
+}
+
+export default Component5;
